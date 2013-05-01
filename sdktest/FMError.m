58daadd9bb4883c6f236d00de41e6d5c675bbffb
James Anthony <james@fuzz.com>
2013-05-01 12:28:26 -0700

    Redo the folder structure so the file system representation matches Xcode groups

--- sdktest/FMError.m
+++
@@ -1,9 +0,0 @@
-//
-//  FMError.h
-//  sdktest
-//
-//  Created by James Anthony on 3/12/13.
-//  Copyright (c) 2013 Feed Media, Inc. All rights reserved.
-//
-
-NSString * const FMAPIErrorDomain = @"FMAPIErrorDomain";
